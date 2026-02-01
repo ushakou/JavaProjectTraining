9db951bd5a107a410d3aeb195e00a044359124d8
admin <111@111>
2026-02-01 09:56:52 +0300

    Lesson 2 edit Task2

--- JavaProjectTraining/src/main/java/com/javaprojecttraining/lesson2/Task2.java
+++ JavaProjectTraining/src/main/java/com/javaprojecttraining/lesson2/Task2.java
@@ -21,7 +21,7 @@ public class Task2 {
 
             if (t > -5) {
                 System.out.println("Warm");
-            } else if (-5 >= t && t >= -20) {
+            } else if (-5 >= t && t > -20) {
                 System.out.println("Normal");
             } else if (-20 >= t) {
                 System.out.println("Cold");
